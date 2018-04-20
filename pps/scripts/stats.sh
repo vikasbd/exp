8ff5a6641e86be1ebad2ea5365e75270927ef481
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2018-04-20 14:00:04 -0700

    stats script fixes

--- pps/scripts/stats.sh
+++ pps/scripts/stats.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -fuxe -o pipefail
+
 INTERVAL="1"  # update interval in seconds
 if [ -z "$1" ]; then
         echo
@@ -10,42 +12,42 @@ if [ -z "$1" ]; then
 fi
 
 IF=$1
-function print_pps()
+function print_pps
 {
-	R1=`cat /sys/class/net/$IF/statistics/rx_packets`
-	T1=`cat /sys/class/net/$IF/statistics/tx_packets`
-	sleep $INTERVAL
-	R2=`cat /sys/class/net/$IF/statistics/rx_packets`
-	T2=`cat /sys/class/net/$IF/statistics/tx_packets`
-	#TXPPS=`expr $T2 - $T1`
-	#RXPPS=`expr $R2 - $R1`
-	#TOTAL=`expr $TXPPS + $RXPPS`
-	TXPPS=$(echo "scale = 3; ($T2 - $T1)/1000000" | bc -l)
-	RXPPS=$(echo "scale = 3; ($R2 - $R1)/1000000" | bc -l)
-	TOTAL=$(echo "scale = 3; $TXPPS + $RXPPS" | bc -l)
-	PPS=`echo "Tx $TXPPS Mpps, Rx $RXPPS Mpps, Total $TOTAL Mpps  |"`
+    TXPPS=$(echo "scale = 3; ($4 - $2)/1000000" | bc -l)
+    RXPPS=$(echo "scale = 3; ($3 - $1)/1000000" | bc -l)
+    TOTAL=$(echo "scale = 3; $TXPPS + $RXPPS" | bc -l)
+    PPS=`echo "Tx $TXPPS Mpps, Rx $RXPPS Mpps, Total $TOTAL Mpps  |"`
 }
 
-function print_bps()
+function print_bps
 {
-   R1=`cat /sys/class/net/$IF/statistics/rx_bytes`
-   T1=`cat /sys/class/net/$IF/statistics/tx_bytes`
-   sleep $INTERVAL
-   R2=`cat /sys/class/net/$IF/statistics/rx_bytes`
-   T2=`cat /sys/class/net/$IF/statistics/tx_bytes`
-   TBPS=`expr $T2 - $T1`
-   RBPS=`expr $R2 - $R1`
-   TXGbps=$(echo "scale = 3; ($TBPS * 8)/1077936128" | bc -l)
-   RXGbps=$(echo "scale = 3; ($RBPS * 8)/1077936128" | bc -l)
+   TBPS=`expr $4 - $2`
+   RBPS=`expr $3 - $1`
+   TXGbps=$(echo "scale = 3; ($TBPS * 8)/1073741824" | bc -l)
+   RXGbps=$(echo "scale = 3; ($RBPS * 8)/1073741824" | bc -l)
    TOTAL=$(echo "scale = 3; $TXGbps + $RXGbps" | bc -l)
    BPS=`echo "Tx $TXGbps Gbps, Rx $RXGbps Gbps, Total $TOTAL Gbps"`
 }
 
+function print_pps_bps()
+{
+   BR1=`cat /sys/class/net/$IF/statistics/rx_bytes`
+   BT1=`cat /sys/class/net/$IF/statistics/tx_bytes`
+   PR1=`cat /sys/class/net/$IF/statistics/rx_packets`
+   PT1=`cat /sys/class/net/$IF/statistics/tx_packets`
+   sleep $INTERVAL
+   BR2=`cat /sys/class/net/$IF/statistics/rx_bytes`
+   BT2=`cat /sys/class/net/$IF/statistics/tx_bytes`
+   PR2=`cat /sys/class/net/$IF/statistics/rx_packets`
+   PT2=`cat /sys/class/net/$IF/statistics/tx_packets`
+   print_pps "$PR1" "$PT1" "$PR2" "$PT2"
+   print_bps "$BR1" "$BT1" "$BR2" "$BT2"
+}
 
 while true
 do
-    print_pps
-    print_bps
+    print_pps_bps
     echo $PPS $BPS
 done
-
+~
